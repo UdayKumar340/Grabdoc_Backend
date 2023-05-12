059266715b2501d181af74a534ba857fc226c2b3
UdayKumar340 <117768248+UdayKumar340@users.noreply.github.com>
2023-05-13 03:09:15 +0530

    Update urls.py

--- GrabdocBackend/patient_app/urls.py
+++ GrabdocBackend/patient_app/urls.py
@@ -6,7 +6,12 @@ urlpatterns = [
     path('mobileregistion/',MobileRegView.as_view()),
     path('verifiy-otp/', verifiyOtp.as_view()),
     path('login/', PatientLoginView.as_view()),
-    path('<int:patient_id>', PatientDetailsUpdate.as_view()),
+    
+
+
+
+
+    path('details/', PatientDetailsUpdate.as_view()),
 
     path('consultantdisease/', ConsultantDiseaseTableView.as_view()),
     path('consultantdisease/<int:consultant_id>/', ConsultantDiseaseTableView.as_view()),
