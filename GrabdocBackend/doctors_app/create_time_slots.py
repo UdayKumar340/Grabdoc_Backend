87472b25c2445da782bee9bd3b0e60d9983c2d9b
UdayKumar340 <117768248+UdayKumar340@users.noreply.github.com>
2024-03-06 00:06:00 +0530

    changes

--- GrabdocBackend/doctors_app/create_time_slots.py
+++ GrabdocBackend/doctors_app/create_time_slots.py
@@ -1,35 +1,47 @@
 
 from django.apps import apps
-DoctorTimeSlots = apps.get_model('patient_app', 'DoctorTimeSlots')
+DoctorTimeSlots = apps.get_model('doctors_app', 'DoctorTimeSlots')
 
-doctor_ids = [12, 40, 36]
+"""
+from datetime import timedelta
+from django.utils import timezone
+
+
+doctor_ids = [12,16,45]
 for doctor_id in doctor_ids:
-    time_slots = []
-
-    for time_slot in time_slots:
+    a=1
+    ds_array=[]
+    current_datetime = timezone.now()
+    for i in range(a):
+        time_slot = timezone.now()
         ds_item = DoctorTimeSlots(doctor_id=doctor_id, time_slot=time_slot)
+        print(DoctorTimeSlots(doctor_id=doctor_id))
         ds_array.append(ds_item)
+        current_datetime += timedelta(minutes =30)#hours=1
 
-    DoctorTimeSlots.objects.bulk_create(ds_array)
+DoctorTimeSlots.objects.bulk_create(ds_array)
 
 #python manage.py shell < ./doctors_app/create_time_slots.py
 
-
 """
-import requests
-import datetime
+
+
+from django.apps import apps
 from datetime import timedelta
-headers = {"Authorization":"Token be2f73bd0506f70b64cb0aa3353554c7942a614a"}
-base_url = "http://localhost:8000/doctors/time-slots/"
-ids = []
-for i in ids:
-    url = f"{base_url}{i}"
-    a=6
-    current_datetime = datetime.datetime.now()
-    for i in range(a):
-        data = {"time_slot":current_datetime.strftime('%Y-%m-%dT%H:%M:%S')}
-        r= requests.post(url,headers=headers,json=data)
-        print(r.content)
-        current_datetime += timedelta(minutes =30)#hours=1
+from django.utils import timezone
 
-"""
+DoctorTimeSlots = apps.get_model('doctors_app', 'DoctorTimeSlots')
+
+doctor_ids = [12, 16, 45]
+time_increment = timedelta(minutes=30)  # Adjust as needed
+
+ds_array = []
+
+for doctor_id in doctor_ids:
+    current_datetime = timezone.now()
+    for _ in range(24):  # Create time slots for 24 hours, adjust as needed
+        ds_item = DoctorTimeSlots(doctor_id=doctor_id, time_slot=current_datetime)
+        ds_array.append(ds_item)
+        current_datetime += time_increment
+
+DoctorTimeSlots.objects.bulk_create(ds_array)
--- GrabdocBackend/patient_app/grabdoc_email.py
+++ GrabdocBackend/patient_app/grabdoc_email.py
@@ -2,11 +2,17 @@ from django.core.mail import send_mail
 
 
 def send(subject,message,to):
-    send_mail(
-        subject,
-        message,
-        "pudaykumar@eitacies.com",
-        [to],
-        fail_silently=False,
-    )
+
+    try:
+
+        send_mail(
+            subject,
+            message,
+            "pudaykumar@eitacies.com",
+            [to],
+            fail_silently=False,
+        )
+    except  Exception as e:
+        print(e)
+        
 
